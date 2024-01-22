2ccedddba01ab7b0c4bc67e7a548e6790c533c1b
小楠 <hello@xiaoNan.cn>
2024-01-22 17:05:00 +0800

    1/22 遇到一个错误，没有在子模块的启动类上添加@ComponentScan("com.xiaonan")，会使子模块无法找到引入模块的切面类以至于没有发生特定日志输出，增加公共模块common，添加日志切面类，增加了公共配置文件，需要放到resources/config

--- train-member/src/main/java/com/xiaonan/train/member/MemberApplication.java
+++ train-member/src/main/java/com/xiaonan/train/member/MemberApplication.java
@@ -4,16 +4,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@ComponentScan("com.xiaonan")
 public class MemberApplication {
     private static final Logger L0G = LoggerFactory.getLogger(MemberApplication.class);
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(MemberApplication.class);
         Environment env = app.run(args).getEnvironment();
         L0G.info("启动成功!!");
-        L0G.info("地址:\thttp://127.0.0.1:{}", env.getProperty("server.port"));
+        L0G.info("地址:\thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"),env.getProperty("server.servlet.context-path"));
 
     }
 }
