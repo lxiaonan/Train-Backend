6b5722ab2d166382c17e982f8f236a7a59abb57b
xiaoNan <hello@xiaoNan.cn>
2024-01-22 15:28:17 +0800

    初始化

---
+++ src/main/java/com/xiaonan/trainbackend/TrainBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xiaonan.trainbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainBackendApplication.class, args);
+    }
+
+}
