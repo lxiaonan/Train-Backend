ff16efc5196c37717c2a8d5b012470bab61b8368
小楠 <hello@xiaoNan.cn>
2024-01-22 16:11:31 +0800

    1/22 修改启动日志，增加/member，修改logback日志

--- train-member/src/main/java/com/xiaonan/train/member/MemberApplication.java
+++ train-member/src/main/java/com/xiaonan/train/member/MemberApplication.java
@@ -1,13 +1,19 @@
 package com.xiaonan.train.member;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class MemberApplication {
-
+    private static final Logger L0G = LoggerFactory.getLogger(MemberApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
-    }
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        L0G.info("启动成功!!");
+        L0G.info("地址:\thttp://127.0.0.1:{}", env.getProperty("server.port"));
 
+    }
 }
